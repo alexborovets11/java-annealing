e39b60ef9f982debb32abfbee652d17f21f1315f
Alex Borovets <borovets.alex@student.greenriver.edu>
2025-04-15 11:19:45 -0700

    hasEmptyString added!

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -29,6 +29,11 @@ public class ProblemSolving {
    * @return true if there is at least one empty string, false otherwise
    */
   public static boolean hasEmptyString(Set<String> words) {
+    for (String word : words) {
+      if (word.isEmpty()) {
+        return true;
+      }
+    }
     return false;
   }
 
